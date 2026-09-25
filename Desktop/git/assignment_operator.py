328cf030a8ae101c28aa19f99f75728dfa659433
Nimra Fatima <n.fatima2029@gmail.com>
2026-09-25 13:08:49 +0500

    assignment operator

---
+++ Desktop/git/assignment_operator.py
@@ -0,0 +1,21 @@
+num = 10
+#num = num + 10
+#  OR
+num1 = num
+num1 += 10
+print("num : ", num1)
+num2 = num
+num2 -= 10
+print("num : ", num2)
+num3 = num
+num3 *= 10
+print("num : ", num3)
+num4 = num
+num4 /= 10
+print("num : ", num4)
+num5 = num
+num5 %= 10
+print("num : ", num5)
+num6 = num
+num6 **= 10 
+print("num : ", num6)
